7ff5c63325b4e975252735eef564c41d3735ba07
Faria Hassan <fariahassan@Farias-MacBook-Pro.local>
2018-08-07 16:08:49 -0700

    search pages using the API json directly

--- static/search.js
+++ static/search.js
@@ -1,7 +1,23 @@
+var tabs = new Array();
+tabs.push('searchResults-product');
+tabs.push('searchResults-video');
+tabs.push('searchResults-blog');
+
+//---------------------------------------------//
+function show(name)
+{
+  var icount = tabs.length;
+  for (var i=0;i<icount;i++)
+  {
+    var tab = $('#'+tabs[i]);
+    console.log('tab is ', tabs[i]);
+  }
+}
+//---------------------------------------------//
 function getResults()
 {
   $.ajaxSetup({ traditional: "true" }); //required else multi parameters go with [] after parm name
-  var term = $('#st-search-input').val();
+  var term = $('#searchBox').val();
 
   var request = $.get('https://api.swiftype.com/api/v1/public/engines/search?engine_key=Uo-nNU7DVc5j98u4RAMf', {
     'q': term
@@ -13,9 +29,34 @@ function getResults()
   //----------------------//
   request.done (function(data) {
     console.log('data is ', data);
+    displayResultsDocumentation(data);
   });
   //----------------------//
   request.always (function() {});
   //----------------------//
 
 }
+//---------------------------------------------//
+function displayResultsDocumentation(data)
+{
+  var pages = data['records']['page'];
+  var countOfPages = Object.keys(pages).length;
+  var container = $('#searchResults-product');
+  for (key in pages)
+  {
+    var page = pages[key];
+    var pageUrl = page['url'];
+    var pageTitle = page['title'];
+    var updatedDate = page['updated_at'];
+    var body = page['body']; //get only the first 50 words or so
+    var content = "<a class='resultTitle' href='"+pageUrl+"'>"+pageTitle+"</a>";
+    content += "<br /><p class='resultBody'>"+body.substring(1,200)+"...</p>";
+    $('<p>', {html: content, class: 'resultRow'}).appendTo(container);
+
+    // console.log ('page is ', page);
+  }
+  // console.log ('pages is ', countOfPages);
+}
+//---------------------------------------------//
+//---------------------------------------------//
+//---------------------------------------------//
