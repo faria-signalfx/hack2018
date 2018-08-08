4d4f1c0aefe269e622b08df3324574edda247405
Faria Hassan <fariahassan@Farias-MacBook-Pro.local>
2018-08-07 18:16:09 -0700

    index now points to search.html

--- static/search.js
+++ static/search.js
@@ -28,6 +28,13 @@ function showTab(evt, cityName) {
 //---------------------------------------------//
 function getResults()
 {
+  getDocumentationResults();
+  // getVideoResults();
+
+}
+//---------------------------------------------//
+function getDocumentationResults()
+{
   $.ajaxSetup({ traditional: "true" }); //required else multi parameters go with [] after parm name
   var term = $('#searchBox').val();
 
@@ -47,7 +54,29 @@ function getResults()
   //----------------------//
   request.always (function() {});
   //----------------------//
+}
+//---------------------------------------------//
+function getVideoResults()
+{
+  $.ajaxSetup({ traditional: "true" }); //required else multi parameters go with [] after parm name
+  var term = $('#searchBox').val();
 
+  var request = $.get('https://api.swiftype.com/api/v1/public/engines/search?engine_key=Uo-nNU7DVc5j98u4RAMf', {
+    'q': term
+  });
+  //----------------------//
+  request.fail (function (jqXHR, textStatus , errorThrown){
+    console.log('error in getting search results: ', textStatus, errorThrown);
+  });
+  //----------------------//
+  request.done (function(data) {
+    console.log('data is ', data);
+    displayResultsDocumentation(data);
+    $('#searchResultsTab-product').click();//FIXME: adding this line causing JS error
+  });
+  //----------------------//
+  request.always (function() {});
+  //----------------------//
 }
 //---------------------------------------------//
 function displayResultsDocumentation(data)
